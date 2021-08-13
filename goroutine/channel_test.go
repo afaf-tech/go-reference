62b68bf2f31c4bfa61aeab269cbd1c9e38d86bec
fatihalfikri5@gmail.com <fatihalfikri5@gmail.com>
2021-08-13 18:33:57 +0700

    [GOROUTINE] range channel

--- goroutine/channel_test.go
+++ goroutine/channel_test.go
@@ -2,6 +2,7 @@ package belajar_golang_goroutine
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -62,22 +63,41 @@ func TestInOutChannel(t *testing.T) {
 /**
 * buffer channel is used for delegate more than one data in a channel
 * defalt buffer when create channel is 1.
-*/
+ */
 func TestBufferedChannel(t *testing.T) {
 	channel := make(chan string, 3) // 3= length of buffer
 	defer close(channel)
 
-	go func(){
+	go func() {
 		channel <- "Fikri"
 		channel <- "Al"
 		channel <- "Fatih"
-	}
+	}()
 
-	go func(){
+	go func() {
 		fmt.Println(<-channel)
 		fmt.Println(<-channel)
 		fmt.Println(<-channel)
-	}
+	}()
 
 	fmt.Println("selesai")
+}
+
+func TestRangeChannel(t *testing.T) {
+
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "perulangan ke " + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	// used to get unkown length of data from channel
+	for data := range channel {
+		fmt.Println("Menerima data", data)
+	}
+
+	fmt.Println("Selesai")
 }
