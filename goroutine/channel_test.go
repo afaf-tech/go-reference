8f4a3da487321c6398991534d79eb0ebd838dd3a
fatihalfikri5@gmail.com <fatihalfikri5@gmail.com>
2021-08-16 08:29:47 +0700

    [GOROUTINE] select channel

--- goroutine/channel_test.go
+++ goroutine/channel_test.go
@@ -26,7 +26,7 @@ func TestCreateChannel(t *testing.T) {
 func GiveMeResponse(channel chan string) {
 	time.Sleep(2 * time.Second)
 
-	channel <- "Ahmad Akmal Fikri"
+	channel <- "Ahmad Fatih"
 }
 
 func TestChannelAsParameter(t *testing.T) {
@@ -83,6 +83,7 @@ func TestBufferedChannel(t *testing.T) {
 	fmt.Println("selesai")
 }
 
+// just for one channel
 func TestRangeChannel(t *testing.T) {
 
 	channel := make(chan string)
@@ -100,4 +101,32 @@ func TestRangeChannel(t *testing.T) {
 	}
 
 	fmt.Println("Selesai")
+}
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari channel 2", data)
+			counter++
+		default:
+			fmt.Println("menunggu data")
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
 }
