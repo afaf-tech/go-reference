c3b6267eee5745b979c2d2355c837fa2f9d4785a
fatihalfikri5@gmail.com <fatihalfikri5@gmail.com>
2021-08-17 16:03:42 +0700

    [GOROUTINE] mutex

---
+++ goroutine/mutex_test.go
@@ -0,0 +1,28 @@
+package belajar_golang_goroutine
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestMutex(t *testing.T) {
+	x := 0
+	var mutex sync.Mutex
+
+	for i := 0; i < 1000; i++ {
+		// mutex: mutual exclusion
+		// lock operation to prevent used by another thread before finished
+		go func() {
+			for j := 1; j <= 100; j++ {
+				mutex.Lock()
+				x = x + 1
+				mutex.Unlock()
+			}
+		}()
+	}
+
+	time.Sleep(time.Second * 5)
+	fmt.Println("Counter  = ", x)
+}
