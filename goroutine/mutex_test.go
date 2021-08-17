22bdea233598099b9d3c38b8863a71a1bef61ea9
fatihalfikri5@gmail.com <fatihalfikri5@gmail.com>
2021-08-17 16:17:41 +0700

    {GOROUTINE] Read Write Mutex

--- goroutine/mutex_test.go
+++ goroutine/mutex_test.go
@@ -26,3 +26,38 @@ func TestMutex(t *testing.T) {
 	time.Sleep(time.Second * 5)
 	fmt.Println("Counter  = ", x)
 }
+
+// Read Write Mutex (RWMutex) prevents race condition in write and write goroutine process
+type BankAccount struct {
+	RWMutex sync.RWMutex //
+	balance int
+}
+
+func (account *BankAccount) AddBalance(amount int) {
+	account.RWMutex.Lock()
+	account.balance = account.balance + amount
+	account.RWMutex.Unlock()
+}
+
+func (account *BankAccount) GetBalance() int {
+	account.RWMutex.RLock()
+	balance := account.balance
+	account.RWMutex.RUnlock()
+	return balance
+}
+
+func TestRWMutex(t *testing.T) {
+	account := BankAccount{}
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			for j := 0; j < 100; j++ {
+				account.AddBalance(1)
+				fmt.Println(account.GetBalance())
+			}
+		}()
+	}
+
+	time.Sleep(time.Second * 5)
+	fmt.Println("total balance", account.GetBalance())
+}
