d5dbcbe4659e793a33b22c5d4e8605e1126cff7f
afaf-tech <fatihalfikri5@gmail.com>
2021-08-22 16:01:01 +0700

    [GOROUTINE] sync.Map

---
+++ goroutine/map_sync_test.go
@@ -0,0 +1,34 @@
+package belajar_golang_goroutine
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+/**
+* all sync package is commonly safe from race condition
+ */
+
+func AddToMap(data *sync.Map, value int, group *sync.WaitGroup) {
+	defer group.Done()
+
+	group.Add(1)
+	data.Store(value, value)
+}
+
+func TestMap(t *testing.T) {
+	data := &sync.Map{}
+
+	group := &sync.WaitGroup{}
+
+	for i := 0; i < 100; i++ {
+		go AddToMap(data, i, group)
+	}
+
+	group.Wait()
+	data.Range(func(key, value interface{}) bool {
+		fmt.Println("key:", key, "value:", value)
+		return true
+	})
+}
