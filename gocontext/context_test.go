838190b7bd2d3fc5ee25668bef4feab0efbaa412
afaf-tech <fatihalfikri5@gmail.com>
2021-09-03 22:36:07 +0700

    [GOCONTEXT] context with deadline

--- gocontext/context_test.go
+++ gocontext/context_test.go
@@ -98,3 +98,26 @@ func TestContextWithTimeout(t *testing.T) {
 	fmt.Println("Total Goroutine", runtime.NumGoroutine())
 
 }
+
+func TestContextWithDeadline(t *testing.T) {
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+
+	parent := context.Background()
+	// will be cancelled if the operation more than 5 seconds.
+	ctx, cancel := context.WithDeadline(parent, time.Now().Add(13*time.Second))
+	defer cancel() // recommended to still use this cancel method
+
+	destination := CreateCounter(ctx)
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+
+	for n := range destination {
+		fmt.Println("Counter", n)
+		// if n == 10 {
+		// 	break
+		// }
+	}
+
+	time.Sleep(1 * time.Second)
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+
+}
