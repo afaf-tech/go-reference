c6f1ffa905047d23f643f3026e455ce9abcc51f2
afaf-tech <fatihalfikri5@gmail.com>
2021-09-04 14:04:59 +0700

    [GOMYSQL] column mapping type

--- gomysql/sql_test.go
+++ gomysql/sql_test.go
@@ -2,6 +2,7 @@ package belajar_golang_database
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"testing"
 )
@@ -40,6 +41,42 @@ func TestQuerySql(t *testing.T) {
 			panic(err)
 		}
 		fmt.Println(id, name)
+	}
+	defer rows.Close()
+	fmt.Println("")
+}
+
+func TestQueryComplexSql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	query := "SELECT id, name, email, rating, birth_date, balance, married, created_at FROM customer"
+	rows, err := db.QueryContext(ctx, query)
+	if err != nil {
+		panic(err)
+	}
+	// loop data with rows.Next() return type bool
+	for rows.Next() {
+		var id, name string
+		var email sql.NullString
+		var balance int32
+		var rating float64
+		var birthDate, createdAt sql.NullTime
+		var married bool
+		err := rows.Scan(&id, &name, &email, &rating, &birthDate, &balance, &married, &createdAt)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("=========================")
+		// cek if email is valid
+		if email.Valid {
+			fmt.Print("email valid	")
+		} else {
+			fmt.Print("email is null	")
+		}
+		fmt.Println(id, name, email, rating, birthDate, balance, married, createdAt)
 	}
 	defer rows.Close()
 	fmt.Println("")
