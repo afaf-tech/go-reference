aee282af3e6554373b68da449320706de8ee03b4
afaf-tech <fatihalfikri5@gmail.com>
2021-09-04 13:15:35 +0700

    [GOMYSQL] query2

--- gomysql/sql_test.go
+++ gomysql/sql_test.go
@@ -20,3 +20,27 @@ func TestExecSql(t *testing.T) {
 
 	fmt.Println("Success inserting db")
 }
+
+func TestQuerySql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	query := "SELECT id, name FROM customer"
+	rows, err := db.QueryContext(ctx, query)
+	if err != nil {
+		panic(err)
+	}
+	// loop data with rows.Next() return type bool
+	for rows.Next() {
+		var id, name string
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(id, name)
+	}
+	defer rows.Close()
+	fmt.Println("")
+}
