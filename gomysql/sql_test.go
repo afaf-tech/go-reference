e4cafc5162a22ee4ecdc1d3e8d9c27a7ab1dfc82
afaf-tech <fatihalfikri5@gmail.com>
2021-09-05 15:37:26 +0700

    [GOMYSQL] db transaction

--- gomysql/sql_test.go
+++ gomysql/sql_test.go
@@ -225,3 +225,38 @@ func TestPrepareStatement(t *testing.T) {
 	}
 
 }
+
+func TestTransaction(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	tx, err := db.Begin()
+	if err != nil {
+		panic(err)
+	}
+
+	// do transaction
+
+	query := "INSERT INTO comments(email, comments) VALUES (?, ?)"
+	for i := 0; i < 10; i++ {
+		email := "fikri" + strconv.Itoa(i) + "@gmail.com"
+		comment := "comment_" + strconv.Itoa(i)
+
+		result, err := tx.ExecContext(ctx, query, email, comment)
+		if err != nil {
+			panic(err)
+		}
+
+		id, err := result.LastInsertId()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("id :", id)
+	}
+
+	err = tx.Rollback()
+	if err != nil {
+		panic(err)
+	}
+}
