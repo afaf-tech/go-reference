695d18926c5e228bd9c0716e97b47ec26f348ebb
afaf-tech <fatihalfikri5@gmail.com>
2021-09-09 19:28:54 +0700

    [GOWEB] test http

---
+++ goweb/http_test.go
@@ -0,0 +1,34 @@
+package belajar_golang_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
+
+// we dont need to run server to test the request. just pass through httptest request
+func TestHttp(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/hello", nil)
+	recorder := httptest.NewRecorder()
+
+	HelloHandler(recorder, request)
+
+	response := recorder.Result()
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	bodyString := string(body)
+	fmt.Println(bodyString)
+
+	assert.Equal(t, "Hello World", bodyString, "Result must be 'Hello World'")
+}
