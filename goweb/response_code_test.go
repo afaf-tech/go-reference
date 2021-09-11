f4586646b60cce66105b836ea2e1e96b9634e125
afaf-tech <fatihalfikri5@gmail.com>
2021-09-11 07:01:57 +0700

    [GOWEB] response code

---
+++ goweb/response_code_test.go
@@ -0,0 +1,47 @@
+package belajar_golang_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func ResponseCode(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "name is empty")
+	} else {
+		fmt.Fprintf(w, "name: %s", name)
+	}
+}
+
+func TestResponseCode(t *testing.T) {
+	request := httptest.NewRequest("GET", "http://localhost:8080/", nil)
+	recorder := httptest.NewRecorder()
+
+	ResponseCode(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(response.StatusCode)
+	fmt.Println(response.Status)
+	fmt.Println(string(body))
+}
+
+func TestResponseCodeValid(t *testing.T) {
+	request := httptest.NewRequest("GET", "http://localhost:8080/?name=Eko", nil)
+	recorder := httptest.NewRecorder()
+
+	ResponseCode(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(response.StatusCode)
+	fmt.Println(response.Status)
+	fmt.Println(string(body))
+}
