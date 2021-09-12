6bfed6464ac1fed85474bbf5c4957fdc83793a7d
afaf-tech <fatihalfikri5@gmail.com>
2021-09-12 10:12:24 +0700

    [GOWEB] nil

--- gobasic/nil.go
+++ gobasic/nil.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// nil just works in interface, function, map, slice, pointer and channel
 func NewMap(name string) map[string]string {
 	if name == "" {
 		return nil
