804064e95b779523f5d5df87b1d62d15b641e23c
afaf-tech <fatihalfikri5@gmail.com>
2021-09-12 10:18:29 +0700

    [GOBASIC] error interface

---
+++ gobasic/interface-error.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func Pembagian(nilai int, pembagi int) (int, error) {
+	if pembagi == 0 {
+		return 0, errors.New("pembagi cannot be 0")
+	} else {
+		result := nilai / pembagi
+		return result, nil
+	}
+}
+func main() {
+	hasil, err := Pembagian(100, 0)
+	if err == nil {
+		fmt.Println("Hasil: ", hasil)
+	} else {
+		fmt.Println(err)
+	}
+}
