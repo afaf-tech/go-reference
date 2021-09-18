95c25a9563e518351e367fb3f31147326e859a8a
afaf-tech <fatihalfikri5@gmail.com>
2021-09-18 09:50:52 +0700

    [GOWEB] XSS cross site scripting

---
+++ goweb/xss_test.go
@@ -0,0 +1,107 @@
+package belajar_golang_web
+
+import (
+	"embed"
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+//go:embed templates/*.gohtml
+var templateAutoEscapeCacheParser embed.FS
+
+// if this code is outside function, it automatically acitivates caching feature in parsing process
+var myTemplatesAutoEscape = template.Must(template.ParseFS(templateAutoEscapeCacheParser, "templates/*.gohtml"))
+
+func TemplateAutoEscape(w http.ResponseWriter, r *http.Request) {
+
+	myTemplatesAutoEscape.ExecuteTemplate(w, "post.gohtml", map[string]interface{}{
+		"Title": "Template Auto escape",
+		"Body":  "<p>Ini adalah body<script> alert('anda di hack')</script></p>",
+	})
+}
+
+func TestTemplateAutoEscape(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateAutoEscape(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TestTemplateAutoEscapeServer(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(TemplateAutoEscape),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TemplateAutoEscapeDisabled(w http.ResponseWriter, r *http.Request) {
+
+	myTemplatesAutoEscape.ExecuteTemplate(w, "post.gohtml", map[string]interface{}{
+		"Title": "Template Auto escape",
+		"Body":  template.HTML("<p>Ini adalah body</p>"),
+	})
+}
+
+func TestTemplateAutoEscapeDisabled(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateAutoEscapeDisabled(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TestTemplateAutoEscapeDisabledServer(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(TemplateAutoEscapeDisabled),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TemplateXSS(w http.ResponseWriter, r *http.Request) {
+
+	myTemplatesAutoEscape.ExecuteTemplate(w, "post.gohtml", map[string]interface{}{
+		"Title": "Template Auto escape",
+		"Body":  template.HTML(r.URL.Query().Get("body")),
+	})
+}
+
+func TestTemplateXSS(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/?body=<p>alert</p>", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateXSS(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TestTemplateXSSServer(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(TemplateXSS),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
