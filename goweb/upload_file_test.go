521522fb90f5c5372a789911a8ca6caa642dea52
afaf-tech <fatihalfikri5@gmail.com>
2021-09-18 10:59:17 +0700

    [GOWEB] download file

--- goweb/upload_file_test.go
+++ goweb/upload_file_test.go
@@ -74,3 +74,27 @@ func TestUploadFile(t *testing.T) {
 	bodyResponse, _ := io.ReadAll(recorder.Result().Body)
 	fmt.Println(string(bodyResponse))
 }
+
+func DownloadFile(w http.ResponseWriter, r *http.Request) {
+	fileName := r.URL.Query().Get("file")
+	if fileName == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "BAD REQUEST")
+		return
+	}
+
+	w.Header().Add("Content-Disposition", "attachment; filename=\""+fileName+"\"")
+	http.ServeFile(w, r, "./resources/"+fileName)
+}
+
+func TestDownloadFile(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(DownloadFile),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
