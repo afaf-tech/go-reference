ae36326b7cd76ba6a6d4252a17b7824d5e3996cc
afaf-tech <fatihalfikri5@gmail.com>
2021-09-18 23:40:09 +0700

    [HTTPROUTER] panic handler

---
+++ httprouter/panic_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPanicHandler(t *testing.T) {
+	router := httprouter.New()
+
+	router.PanicHandler = func(w http.ResponseWriter, r *http.Request, error interface{}) {
+		fmt.Fprint(w, "Panic: ", error)
+	}
+	// create endpoints
+	router.GET("/", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		panic("ups")
+	})
+
+	request := httptest.NewRequest("GET", "http://localhost:8080/", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+	response := recorder.Result()
+
+	bytes, _ := io.ReadAll(response.Body)
+	assert.Equal(t, "Panic: ups", string(bytes))
+}
