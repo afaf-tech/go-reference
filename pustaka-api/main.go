92b0ab8b05b17c7cc634571c96837f1f1c481075
afaf-tech <fatihalfikri5@gmail.com>
2021-09-22 05:54:41 +0700

    [PUTAKA-API] handler

--- pustaka-api/main.go
+++ pustaka-api/main.go
@@ -9,18 +9,22 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"name": "afaf",
-			"bio":  "a software engineer and self-employed",
-		})
-	})
-	router.GET("/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"name": "hello afaf",
-			"bio":  "learing golang with agung setiawan mu",
-		})
-	})
+	router.GET("/", rootHandler)
+	router.GET("/hello", helloHandler)
 
 	router.Run(":3000")
+}
+
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "afaf",
+		"bio":  "a software engineer and self-employed",
+	})
+}
+
+func helloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "hello afaf",
+		"bio":  "learing golang with agung setiawan mu",
+	})
 }
