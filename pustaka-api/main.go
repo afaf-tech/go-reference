1a83aa92754c25dd9feee2ba78ed1f693f8979b6
afaf-tech <fatihalfikri5@gmail.com>
2021-09-22 06:21:07 +0700

    [PUSTAKA-API] complex routing

--- pustaka-api/main.go
+++ pustaka-api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -11,6 +12,9 @@ func main() {
 
 	router.GET("/", rootHandler)
 	router.GET("/hello", helloHandler)
+	router.GET("/books/:id", bookHandler)
+	router.POST("/books", postBookHandler)
+	router.GET("/query", queryHandler)
 
 	router.Run(":3000")
 }
@@ -26,5 +30,39 @@ func helloHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"name": "hello afaf",
 		"bio":  "learing golang with agung setiawan mu",
+	})
+}
+
+func bookHandler(c *gin.Context) {
+	id := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{
+		"name": "hello afaf " + id,
+		"bio":  "learing golang with agung setiawan mu",
+	})
+}
+
+type BookInput struct {
+	Title string `json:"title"`
+	Price int    `json:"price"`
+}
+
+func postBookHandler(c *gin.Context) {
+	var bookInput BookInput
+
+	err := c.ShouldBindJSON(&bookInput)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.JSON(http.StatusOK, bookInput)
+}
+
+func queryHandler(c *gin.Context) {
+	id := c.Query("id")
+	price := c.Query("price")
+	c.JSON(http.StatusOK, gin.H{
+		"name":  "hello afaf " + id,
+		"price": price,
+		"bio":   "learing golang with agung setiawan mu",
 	})
 }
