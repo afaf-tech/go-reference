c2ffb328ea1293b36dd7a87b46ac67ce7245cb12
afaf-tech <fatihalfikri5@gmail.com>
2022-10-15 20:02:22 +0700

    validation variables

--- go-validation/validator_test.go
+++ go-validation/validator_test.go
@@ -1,14 +1,36 @@
 package govalidation
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/go-playground/validator/v10"
 )
 
+var validate *validator.Validate = validator.New()
+
 func TestValidati(t *testing.T) {
-	validate := validator.New()
 	if validate == nil {
 		t.Error("Validate is nil")
 	}
+
+}
+
+func TestValidationVariable(t *testing.T) {
+	var user string = ""
+	err := validate.Var(user, "required")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func TestValidateTwoVariables(t *testing.T) {
+	password := "password"
+	confirmPassword := "password"
+
+	err := validate.VarWithValue(password, confirmPassword, "eqfield")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
 }
