87e7b90689d844779fad74f6a15c8a73a47dcd47
afaf-tech <fatihalfikri5@gmail.com>
2022-10-16 12:33:57 +0700

    validation: collection and map

--- go-validation/validator_test.go
+++ go-validation/validator_test.go
@@ -122,3 +122,123 @@ func TestValidationNestedStruct(t *testing.T) {
 		}
 	}
 }
+
+func TestValidationDive(t *testing.T) {
+	type Address struct {
+		City   string `validate:"required"`
+		Street string `validate:"required"`
+	}
+
+	type User struct {
+		Id      string    `validate:"required"`
+		Name    string    `validate:"required"`
+		Address []Address `validate:"required,dive"`
+	}
+
+	akira := &User{
+		Id:   "32",
+		Name: "akira",
+		Address: []Address{
+			{
+				City:   "akira",
+				Street: "jl veteran",
+			},
+			{
+				City:   "",
+				Street: "",
+			},
+		},
+	}
+
+	err := validate.Struct(akira)
+	if err != nil {
+		validationErrors := err.(validator.ValidationErrors)
+		for _, err := range validationErrors {
+			fmt.Println("Field error:", err.Field(), "on tag", err.Tag(), "error", err.Error())
+		}
+	}
+}
+
+func TestValidationBasicColletion(t *testing.T) {
+	type Address struct {
+		City   string `validate:"required"`
+		Street string `validate:"required"`
+	}
+
+	type User struct {
+		Id      string    `validate:"required"`
+		Name    string    `validate:"required"`
+		Address []Address `validate:"required,dive"`
+		Hobbies []string  `validate:"dive,required,min=3"` // hobbies is not required because of no "required" at the first order
+	}
+
+	akira := &User{
+		Id:   "32",
+		Name: "akira",
+		Address: []Address{
+			{
+				City:   "akira",
+				Street: "jl veteran",
+			},
+			{
+				City:   "",
+				Street: "",
+			},
+		},
+		Hobbies: []string{"na", "nilna"},
+	}
+
+	err := validate.Struct(akira)
+	if err != nil {
+		validationErrors := err.(validator.ValidationErrors)
+		for _, err := range validationErrors {
+			fmt.Println("Field error:", err.Field(), "on tag", err.Tag(), "error", err.Error())
+		}
+	}
+}
+
+func TestValidationMap(t *testing.T) {
+	type Address struct {
+		City   string `validate:"required"`
+		Street string `validate:"required"`
+	}
+
+	type School struct {
+		Name string `validate:"required"`
+	}
+
+	type User struct {
+		Id      string            `validate:"required"`
+		Name    string            `validate:"required"`
+		Address []Address         `validate:"required,dive"`
+		Hobbies []string          `validate:"dive,required,min=3"`                   // hobbies is not required because of no "required" at the first order
+		Schools map[string]School `validate:"dive,keys,required,min=2,endkeys,dive"` // keys for key validations, last dive is for School validation
+		Wallet  map[string]int    `validate:"dive,keys,required,min=2,endkeys,required,gt=0"`
+	}
+
+	akira := &User{
+		Schools: map[string]School{
+			"SD": {
+				Name: "Mi Maarif tanqul",
+			},
+			"SMP": {
+				Name: "Mts Maarif tanqul",
+			},
+			"": {
+				Name: "",
+			},
+		},
+		Wallet: map[string]int{
+			"GOPAY": 10,
+			"Dana":  -1,
+		},
+	}
+
+	err := validate.Struct(akira)
+	if err != nil {
+		validationErrors := err.(validator.ValidationErrors)
+		for _, err := range validationErrors {
+			fmt.Println("Field error:", err.Field(), "on tag", err.Tag(), "error", err.Error())
+		}
+	}
+}
